f167578c219d9cc661161188812671cc942519d2
Tyler Finleyy <tylerfinley1@Tylers-MacBook-Pro.local>
2015-09-11 10:23:55 -0700

    add files to allow functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
-var findReplace = function(find,replace){
+var findReplace = function(input, find, replace) {
+  var re = new RegExp(find, 'g');
+  var input = input.replace(re, replace);
+  return input;
+};
 
-}
+
+
+$(document).ready(function() {
+  $('form#find_replace').submit(function(event) {
+    var input = $('input#sentence').val();
+    var find = $('input#find').val();
+    var replace = $('input#replace').val();
+    var result = findReplace(input, find, replace);
+
+    $('.new-sentence').text(result);
+    $('#result').show();
+    event.preventDefault();
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,9 @@
 describe('findReplace', function() {
-  it('will find and replace a word from a string',function() {
-    expect(findReplace('red')).to.equal('blue');
+  it('will find and replace a word from a string', function() {
+    expect(findReplace('red', 'red', 'blue')).to.equal('blue');
+  });
+
+  it('will find and replace words from a string', function() {
+    expect(findReplace('red rover red rover', 'rover', 'range rover')).to.equal('red range rover red range rover');
   });
 });
